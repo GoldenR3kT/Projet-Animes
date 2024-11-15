dae49ae98bf6c067522954e799eff5a70b6db38e
Ilyane <ilyane.selmane@etu.unice.fr>
2024-11-15 14:20:00 +0100

    Ajout de Vuetify au projet

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 import './style.css'
-import App from './App.vue'
+import App from './App.vue';
+import { createVuetify } from 'vuetify';
+import * as components from 'vuetify/components';
+import * as directives from 'vuetify/directives';
+import 'vuetify/styles'; // Importer les styles de base
+import '@mdi/font/css/materialdesignicons.css'; // (Facultatif) Importer les icônes
 
-createApp(App).mount('#app')
+const vuetify = createVuetify({
+    components,
+    directives,
+});
+
+const app = createApp(App);
+app.use(vuetify);
+app.mount('#app');
